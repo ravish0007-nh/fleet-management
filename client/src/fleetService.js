2322a8c586e0ac6af07fda778e18e5229ca91bf7
ravish0007-nh <ravish@nighthack.in>
2023-02-22 11:47:12 +0530

    Adds axios client dependency and api service file

---
+++ client/src/fleetService.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:3000/api/v1'
+
+class FleetService {
+  static async getMachines() {
+    try {
+      const response = await axios.get(`${BASE_URL}/machines`) 
+      return response.data
+    }
+    catch (error) {
+      console.log(error);
+      return []
+    }
+  }
+
+  static async getLocations() {
+    try {
+      const response = await axios.get(`${BASE_URL}/locations`) 
+      return response.data
+    }
+    catch (error) {
+      console.log(error);
+      return []
+    }
+  }
+}
+
+export default FleetService
+
+
